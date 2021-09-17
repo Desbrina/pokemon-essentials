c8613bc89ed5e81ea47fb2261071e7bc64f181c9
Desbrina <cez.turner@ntlworld.com>
2021-09-17 16:07:05 +0100

    Update 002_New_Items.rb
    Shiny option done

--- Data/Scripts/998_Custom/002_New_Items.rb
+++ Data/Scripts/998_Custom/002_New_Items.rb
@@ -51,3 +51,19 @@ ItemHandlers::UseInField.add(:POCKETPC,proc{|item|
   pbPokeCenterPC
   next 1
 })
+
+ItemHandlers::UseOnPokemon.add(:SHIABERRY,proc { |item,pkmn,scene|
+   #next pbHPItem(pkmn,10,scene)
+   # Changes: SHINY, IV, Hidden Ability, Pokerus
+   chance = 1 #rand(1,5)
+   if chance == 1
+        if pkmn.shiny?
+           scene.pbDisplay(_INTL("It won't have any effect."))
+        else
+           scene.pbDisplay(_INTL("{1} became shiny.",pkmn.name))
+           pkmn.shiny = true
+        end
+   end
+   pkmn.calc_stats
+   next 1
+})
