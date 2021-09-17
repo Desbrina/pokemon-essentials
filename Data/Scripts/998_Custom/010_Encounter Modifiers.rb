d0d43c64471e43e538f55f4dedf452c5b773f2b0
Desbrina <cez.turner@ntlworld.com>
2021-09-17 12:58:01 +0100

    Update 010_Encounter Modifiers.rb
    Tidied up encounter modifier comments

--- Data/Scripts/998_Custom/010_Encounter Modifiers.rb
+++ Data/Scripts/998_Custom/010_Encounter Modifiers.rb
@@ -1,10 +1,12 @@
+# VIVILLON Pattern
 Events.onWildPokemonCreate+=proc {|sender,e|
    pokemon=e[0]
-   if $game_switches[101] # 105 = VIVILLON Pattern
+   if $game_switches[101]
      pokemon.form=  $game_variables[105]
    end
 }
 
+# Shadow Pokemon
 Events.onWildPokemonCreate+=proc {|sender,e|
    pokemon=e[0]
    pokemon.makeShadow if $game_switches[103]
